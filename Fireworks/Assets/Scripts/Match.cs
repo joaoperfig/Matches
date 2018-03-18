9e0240a29865b18b486fc2ed0f110b743d0769e9
joaoperfig <joaoperfig@gmail.com>
2018-03-18 02:22:56 +0000

    add matchbox spawning

---
+++ Fireworks/Assets/Scripts/Match.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Match : MonoBehaviour {
+
+	public float walkspeed;
+
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = new Vector3 (0, 0, 0);
+		if (Input.GetKey (KeyCode.A)) { // Player pressed left
+			movement += new Vector3 (-1, 0, 0);
+		}
+		if (Input.GetKey (KeyCode.D)) { // Player pressed Right
+			movement += new Vector3 ( 1, 0, 0);
+		}
+		if (movement.sqrMagnitude > 0) {
+			if (movement.x < 0) gameObject.transform.localScale = new Vector3 (-2f, 2f, 2f);
+			else gameObject.transform.localScale = new Vector3 ( 2f, 2f, 2f);
+			animator.SetTrigger ("walk");
+			movement.Normalize ();
+			movement = movement * walkspeed * Time.deltaTime;
+			gameObject.transform.position = gameObject.transform.position + movement;
+		} else {
+			animator.SetTrigger ("stop");
+		}
+	}
+}
---
+++ Fireworks/Assets/Scripts/MatchBox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchBox : MonoBehaviour {
+
+	public GameObject match;
+	public Vector3 deltaspawn;
+
+	private Animator anim;
+	private bool isbirthing = false;
+	private float maketime;
+
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<Animator> ();
+		makeMatch ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (isbirthing) {
+			if (anim.GetCurrentAnimatorStateInfo (0).IsTag ("idle") && Time.time > maketime+0.02) {
+				Debug.Log ("newmatch");
+				GameObject.Instantiate (match, gameObject.transform.position + deltaspawn, Quaternion.identity);
+				isbirthing = false;
+			}
+		}
+	}
+
+	public void makeMatch(){
+		isbirthing = true;
+		anim.SetTrigger ("birth");
+		maketime = Time.time; // Prevents match spawning because update was called before animation trigger was processed
+	}
+}
